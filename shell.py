88cc419321604732945185a0f6c0947d2203a307
Ahmad-Solikhin-GR <63105261+Ahmad-Solikhin-GR@users.noreply.github.com>
2021-07-07 01:47:19 +0700

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,17 @@
+import basic
+print("")
+print("Yōkoso!!!")
+print("Shiawasena shiyō!!!")
+print("")
+
+while True:
+	text = input('kiso > ')
+	if text.strip() == "": continue
+	result, error = basic.run('<stdin>', text)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
